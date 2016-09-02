2ccac4fa4f7864db46ff794850d0d9825d55d795
Edward Cree <ec429@cantab.net>
2016-09-02 04:13:46 +0100

    Add Delta II and Delta IV

--- rocket.py
+++ rocket.py
@@ -6,6 +6,12 @@ class RocketFamily(object):
         self.launches = launches
         self.failures = failures
 
+# Failure counting rules:
+# If the primary payload reached its target orbit, it's a success, regardless
+# of "anomalies" or "secondary payload failures".
+# Most of these numbers are copied from Wikipedia infoboxes and haven't been
+# checked.
+
 Falcon9 = RocketFamily(29, 2, 'Falcon 9')
 Shuttle = RocketFamily(135, 2, 'Shuttle')
 ProtonM = RocketFamily(98, 10, 'Proton-M')
@@ -14,7 +20,9 @@ SoyuzFG = RocketFamily(56, 0, 'Soyuz FG')
 SoyuzU = RocketFamily(784, 20, 'Soyuz U')
 AtlasV = RocketFamily(64, 1, 'Atlas V')
 Ariane5 = RocketFamily(87, 4, 'Ariane 5')
+DeltaII = RocketFamily(153, 2, 'Delta II')
+DeltaIV = RocketFamily(33, 0, 'Delta IV') # 1 partial failure, but primary payload reached target orbit
 
-families = [Falcon9, Shuttle, ProtonM, Soyuz2, SoyuzFG, SoyuzU, AtlasV, Ariane5]
+families = [Falcon9, Shuttle, ProtonM, Soyuz2, SoyuzFG, SoyuzU, AtlasV, Ariane5, DeltaII, DeltaIV]
 
 by_name = dict((rkt.name, rkt) for rkt in families)
